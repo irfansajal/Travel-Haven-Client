3bcec1f9cc6ce65ecf1a84d2a61fc4260fefe3fb
irfansajal <irfan@Irfans-MacBook-Pro.local>
2023-09-07 17:37:06 +0200

    continue progress

--- src/Components/Catagories/Categories.jsx
+++ src/Components/Catagories/Categories.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Container from '../Shared/Container/Container';
 import { categories } from './catagoriesData';
 import CategoryBox from './CategoryBox/CategoryBox';
 
 const Categories = () => {
+    const [params,setParams] = useSearchParams()
+    const value = params.get('category')
+   // console.log(value);
     return (
         <Container>
             <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,22 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 import Container from '../Shared/Container/Container';
+import Loader from '../Shared/Loader';
+import Card from './Card';
 
 const Rooms = () => {
-    const [rooms,setRooms] = useState([])
-    useEffect(() =>{
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
+   // console.log(category);
+
+    const [rooms, setRooms] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        /* setLoading(true) modified as it makes really shaky when website is rendering */
+        
         fetch('./Rooms.json')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setRooms(data)
-        })
-        .catch(err => console.log(err))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                if (category) {
+                    const filtered = data.filter(room => room.category === category)
+                    setRooms(filtered)
+                    console.log(filtered);
+                }
+
+                else {
+                    setRooms(data)
+                   
+                    // console.log(data);
+                   
+                }
+                setLoading(false)
+            })
+            .catch(err => console.log(err))
+    }, [category])
+    //Loading system
+    if (loading) {
+        return <Loader></Loader>
+    }
     return (
         <Container>
-           <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-            {rooms.map(room => <p>{room.location}</p>)}
-           </div>
+           {
+            rooms && rooms.length > 0 ?  <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            {rooms.map((room, index) => (
+                <Card key={index} room={room}></Card>
+            ))}
+        </div>: (
+            <div className=' pt-60'>
+                <Heading
+        title='Currently no Rooms available in this Category!'
+        subtitle='Please select other Categories for now'
+        center = {true}
+        >
+
+        </Heading>
+            </div>
+        )
+           }
         </Container>
     );
 };
